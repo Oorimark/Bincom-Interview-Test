0d6aacc7932d4d6d2d5942af97185aa9f0114f9d
Oorimark <91107301+Oorimark@users.noreply.github.com>
2022-03-12 21:19:01 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 require('dotenv');
 
 const db = mysql.createConnection({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+    host : process.env.DB_HOST || "mysql-71846-0.cloudclusters.net",
+    user : process.env.DB_USER  || "markbincom",
+    password : process.env.DB_PASSWORD || "LwqrO6YF",
+    database : process.env.DB_NAME || "bincom_test"
 });
 
 db.connect((err) => { if(err) throw err; else console.log('SQL_db connected') });
